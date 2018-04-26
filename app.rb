eedc69bafa77c3e8279fb46caa136467625de95b
Philip Van Raalte <phil.vanraalte@gmail.com>
2018-04-25 20:40:08 -0400

    use create-react-app

--- app.rb
+++ app.rb
@@ -2,8 +2,10 @@ require 'sinatra'
 
 set :bind, '0.0.0.0'
 set :port, 80
+set :public_folder, './react-app/build/'
 
 get '/*' do
     filepath = File.join(settings.public_folder, 'index.html')
     File.read(filepath)
 end
+ 
